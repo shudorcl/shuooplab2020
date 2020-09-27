1b0b7511afb180bceef0bf8fb85b7da28311400e
SYYANI <wunongfeidao@live.com>
2020-09-27 14:43:43 +0800

    gf

---
+++ string/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  string
+//
+//  Created by SUNYIYAN on 2020/9/27.
+//  Copyright © 2020 SUNYIYAN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
