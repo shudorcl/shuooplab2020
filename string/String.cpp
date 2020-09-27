0c09a0eab3b2957ed5950c520b41914f5037fbb0
shudorcl <1985366171@qq.com>
2020-09-27 15:07:17 +0800

    -test -copy

--- string/String.cpp
+++ string/String.cpp
@@ -3,12 +3,16 @@
 #include "String.h"
 
 using namespace std;
-
+String::String(const char *s)
+{
+    str = new char[strlen(s) + 1];
+    strcpy(str, s);
+}
 int String::find(const String &Str) const
 {
     int i;
 }
 void String::copy(const String &Str) const
 {
-    strcpy(str,Str.str);
+    strcpy(str, Str.str);
 }
--- string/test.cpp
+++ string/test.cpp
@@ -1,10 +1,13 @@
 #include "String.h"
-
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    String test("Ark"),str;
+    int n;
+    String test("Ark"), str;
     str.copy(test);
+    cout << "Wait...";
+    cin >> n;
     return 0;
 }
