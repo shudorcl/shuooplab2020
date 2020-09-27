b292b8685c5cbef9440db4c5dea84b2fb8db65cf
shudorcl <1985366171@qq.com>
2020-09-27 10:58:54 +0800

    copy

--- string/String.cpp
+++ string/String.cpp
@@ -4,8 +4,11 @@
 
 using namespace std;
 
-int String::find(const String &str) const
+int String::find(const String &Str) const
 {
     int i;
 }
-
+void String::copy(const String &Str) const
+{
+    strcpy(str,Str.str);
+}
