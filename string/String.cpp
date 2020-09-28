c153fe60427d39f167b7df895dfea390e824d742
SYYANI <wunongfeidao@live.com>
2020-09-28 22:13:10 +0800

    add append and empty

--- string/String.cpp
+++ string/String.cpp
@@ -3,58 +3,134 @@
 #include "String.h"
 
 using namespace std;
+//the defination of construct function
 String::String(const char *s)
 {
-    str = new char[strlen(s) + 1];
-    strcpy(str, s);
+    str=new char[strlen(s)+1];
+    strcpy(str,s);
 }
-int String::find(const String &Str) const
+
+String::String(const char *s,int n)
 {
-    int i, j, m, n, flag;
-    m = strlen(Str.str);
-    n = strlen(str);
-    if (m > n)
-        return -1;
-    for (i = 0; i < n - m; i++)
+    int i,m=(int)strlen(s);
+    if(m<n)
+        n=m;
+    if(n<0)
+        n=0;
+    str=new char[n+1];
+    for(i=0;i<n;i++)
+        str[i]=s[i];
+    str[i]='\0';
+}
+
+String::String(int n,char c)
+{
+    int i;
+    if(n<0)
+        n=0;
+    str=new char[n+1];
+    for(i=0;i<n;i++)
+        str[i]=c;
+    str[i]='\0';
+}
+String::String(const String &Str)
+{
+    str =new char[strlen(Str.str)+1];
+    strcpy(str,Str.str);
+}
+
+String::String(const String &Str,int pos,int n)
+{
+    int i,m;
+    m=(int)strlen(Str.str);
+    if(pos>m)
     {
-        flag = 1;
-        for (j = 0; j < m; j++)
-        {
-            if (Str.str[j] != str[i + j])
-            {
-                flag = 0;
-                break;
-            }
-        }
-        if (flag)
-            return i;
+        str=new char[1];
+        str[0]='\0';
+        return;
     }
-    return -1;
+    if(m-pos<n)
+        n=m-pos;
+    if(n<0)
+        n=0;
+    str =new char[n+1];
+    for(i=0;i<n;i++)
+        str[i]=Str.str[pos+i];
+    str[i]='\0';
 }
-int String::rfind(const String &Str) const
+
+//the defination of append
+String &String::append(const String &s)
 {
-    int i, j, m, n, flag;
-    m = strlen(Str.str);
-    n = strlen(str);
-    if (m > n)
-        return -1;
-    for (i = n - m - 1; i >= 0; i--)
+    strcat(str, s.str);
+    m_str += strlen(s.str);
+    return *this;
+}
+
+String &String::append(const String &s, int pos, int n)
+{
+    //setting the begin and the end of the target
+    char temp[100];
+    int k = 0;
+    for(int i = pos ;i < n ;i++)
     {
-        flag = 1;
-        for (j = m - 1; j >= 0; j--)
-        {
-            if (Str.str[j] != str[i + j])
-            {
-                flag = 0;
-                break;
-            }
-        }
-        if (flag)
-            return i;
+        temp[k] = s.str[i];
+        k++;
     }
-    return -1;
+    strcat(str,temp);
+    m_str += strlen(temp);
+    return * this;
 }
-void String::copy(const String &Str) const
+
+String & String::append(const char *s)
 {
-    strcpy(str, Str.str);
+    strcat(str,s);
+    m_str += strlen(s);
+    return *this;
 }
+
+String &String::append(int pos, const char s)
+{
+    char p_temp[100];
+    int i = 0;
+    for(i = 0;i < pos ; i++)
+    {
+        p_temp[i] = s;
+    }
+    p_temp[i] = '\0';
+    strcat(str, p_temp);
+    m_str += strlen(p_temp);
+    return *this;
+}
+
+//the functiong of the empty
+bool String::empty() const
+{
+    if(str == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+//the function of insert
+String & String::insert(int p0,const char *s)
+{
+    int n=(int)strlen(str);
+    if(p0>n)
+        p0=n;
+    char *p=new char[strlen(str)+strlen(s)+1];
+    strncpy(p,str,p0);
+    p[p0]='\0';
+    strcat(p,s);
+    strcat(p,str+p0);
+    delete [] str;
+    str=p;
+    return *this;
+}
+
+//the function of substr
+
--- string/String.h
+++ string/String.h
@@ -1,28 +1,33 @@
 #ifndef STRING_FUNCTION_STRING_HEAD_H
 #define STRING_FUNCTION_STRING_HEAD_H
+using namespace std;
+
 class String
 {
 private:
     char *str;
-
+    int m_str;//the length of the String
 public:
     String(const char *s = "");
     String(const char *s, int n);
     String(int n, char c);
     String(const String &Str);
     String(const String &Str, int pos, int n);
-    ~String() {}//
+    ~String() {}
+    
     //String Function
-    int length() const;
-    int find(const String &Str) const;
-    int rfind(const String &Str) const;
-    void swap(String &Str);
-    void reverse(String &Str);
-    void uppercase(String &Str);
-    void lowercase(String &Str);
-    void copy(const String &Str) const;
+    //workingspace for sunyiyan
+    String& append(const String &s);
+    String& append(const String &s,int pos,int n);
+    String& append(const char * test);
+    String& append(int pos , const char test);
+    
+    bool empty() const;
+    
+    String & insert(int p0,const char *s);
+    
 };
 #endif //STRING_FUNCTION_STRING_HEAD_H
-//xhange
-//sunyiyan.change
-//haoye
+
+
+
