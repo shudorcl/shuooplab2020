40f65c96dddafe9b993b126c8ae6ce8e56d1fccc
wyhallenwu <67626395+wyhallenwu@users.noreply.github.com>
2020-10-27 18:05:48 +0800

    Update vectest.cpp

--- vectest.cpp
+++ vectest.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
-#include "vector.h"
+#include "vec.h"
 using namespace std;
 int main() {
     //测试构造函数
-    Vector<double> v2(1,10);
-    Vector<double> v4(1,9);
-    Vector<double> v3(3,9);
-    Vector<double> v1(3,10);
-    Vector<double> v5(v1);//测试拷贝构造函数
-    Vector <int> v9;
-    cout<<v2;
-    cout<<v2.module()<<endl;//测试module求模函数
-    v2+=2;//测试+=
-    cout<<v2;
-    cout<<v2.module()<<endl;
-    v2.normalize();//测试normalize单位化函数
-    cout << v2;
-    try {
-        cout << v2 * v3;
-    } catch (int ) {
-        cout<<"size over"<<endl;
-    }//异常处理的测试
-    cout<<v2*v1;//测试向量相乘
-    cout<<(v4!=v3)<<endl;//测试逻辑运算符函数
-    cout<<v5.empty()<<endl;//测试empty函数
-    v5=3+v1;//测试友元函数+
-    cout<<v5;
-    cout<<v9;
+    Vector<double> v1(1.5,10);
+    Vector<double> v2(3,10);
+    Vector<int>v3(0);//空向量
+    cout<<v1<<v2;//测试重载输出流
+    cout<<v1+v2;//测试重载+
+    cout<<v1*v2;//测试重载叉乘
+    cout<<(-1)*v2;//测试友元函数
+    cout<<v2-v1;//测试重载-
+    cout<<(v1==v2)<<endl;//测试重载逻辑运算符
+    cout<<v2.empty()<<endl;//测试判断空向量函数
+    v2.normalize();//向量单位化
+    cout<<v2<<endl;
+    v2+=2;//测试迭代运算符
+    cout<<v2<<endl;
+    cout<<v2[6]<<endl;//测试重载[]
+    cout<<v2.module()<<endl;//求模长
+    cout<<v3.empty()<<endl;//判断空向量测试
+    v3.vdimup(3);//测试向量升维
+    cout<<v3;
+    v3.vdimdown(1);//测试向量降维
+    cout<<v3;
+    cout<<v1.multiply(v2)<<endl;//测试点乘
+    cout<<v1.angle(v2)<<"angle"<<endl;//测试角度计算
+
     return 0;
 }
