8392076cc698d46cf3e8643dacec339db712c5dc
wyhallenwu <67626395+wyhallenwu@users.noreply.github.com>
2020-10-27 18:05:20 +0800

    Update vec.h

--- vec.h
+++ vec.h
@@ -1,9 +1,9 @@
 //
-// Created by wyh on 2020/10/22.
+// Created by 14105 on 2020/10/27.
 //
 
-#ifndef XIANGMUSHIYAN_VECTOR_H
-#define XIANGMUSHIYAN_VECTOR_H
+#ifndef VECFINAL_VEC_H
+#define VECFINAL_VEC_H
 #include <iostream>
 #include <string>
 #include <cmath>
@@ -15,14 +15,18 @@ private:
     int size;
 public:
     //构造函数和析构函数
-    Vector(int vdim=1);
-    Vector(T para,int vdim);
-    Vector(const Vector &ve);
+    Vector(int vdim=1);//输入维数自动初始化为0
+    Vector(T para,int vdim);//输入初始化值和维数
+    Vector(const Vector &ve);//拷贝构造函数
     virtual ~Vector();
     //功能
     bool empty()const;//判断空向量
     T module()const;//求模长
     void normalize();//单位化
+    void vdimup(const int x);//维数增加，多出部分自动补0
+    void vdimdown(const int x);//维数减少，多于直接删除
+    double angle(const Vector<T> &ve);//角度计算
+    T multiply(const Vector<T> &ve)throw(int);//点乘
     //运算符的重载
     Vector operator+(const Vector<T> &ve)throw(int);//向量与向量相加
     Vector operator+(const T s);
@@ -30,7 +34,7 @@ public:
     Vector operator-(const Vector<T> &ve);
     Vector & operator+=(Vector<T> &ve)throw(int);
     Vector & operator+=(const T s);
-    Vector & operator=(const Vector<T> & ve);
+    Vector & operator=(const Vector<T> & ve);//深赋值
     bool operator!=(const Vector<T> &ve);
     bool operator==(const Vector<T> &ve);
     Vector operator*(const Vector<T> &ve)throw(int);
@@ -40,7 +44,7 @@ public:
         return ve*s;
     }
     Vector & operator*=(const T n);
-    T operator[](const int index)throw(int);
+    T operator[](const int index)throw(int);//重载[]
     friend ostream & operator<<(ostream &out,const Vector<T> &ve)//重载输出流
     {
         out<<"[ ";
@@ -67,11 +71,11 @@ Vector<T>::Vector(T para, int vdim):num(new T[vdim]),size(vdim)//带默认值的
         num[i]=para;
 }
 template <typename T>
-Vector<T>::Vector(const Vector<T> &ve)//拷贝构造
+Vector<T>::Vector(const Vector<T> &ve)//深拷贝构造函数
 {
     num=new T[ve.size];
     size=ve.size;
-    for(int i=0;i<ve.size;i++)
+    for(int i=0;i<size;i++)
         num[i]=ve.num[i];
 }
 
@@ -85,15 +89,15 @@ Vector<T>::~Vector()
 //运算符重载
 
 template<typename T>
-Vector<T> Vector<T>::operator+(const Vector<T> & ve)throw(int)//重载+运算符-向量相加
+Vector<T> Vector<T>::operator+(const Vector<T> & ve)throw(int)//重载+运算符向量相加
 {
-        if(this->size!=ve.size)
-            throw 1;
-        Vector<T> sum(size);
-        for(int i=0;i<this->size;i++) {
-            sum.num[i]=this->num[i] + ve.num[i];
-        }
-        return sum;
+    if(this->size!=ve.size)
+        throw (int)1;
+    Vector<T> sum(size);
+    for(int i=0;i<this->size;i++) {
+        sum.num[i]=this->num[i] + ve.num[i];
+    }
+    return sum;
 
 }
 template <typename T>
@@ -129,8 +133,8 @@ Vector<T> Vector<T>::operator*(const Vector<T> &ve) throw(int)//重载点乘
 {
     if(this->size!=ve.size)
         throw (int)1;
-    Vector<T> mul(size);
-    for(int i=0;i<size;i++)
+    Vector<T> mul(this->size);
+    for(int i=0;i<this->size;i++)
     {
         mul.num[i]=this->num[i]*ve.num[i];
     }
@@ -156,7 +160,12 @@ Vector<T> & Vector<T>::operator*=(const T n)//重载向量*=向量
 template <typename T>
 Vector<T> Vector<T>::operator-(const Vector<T> &ve)//重载-
 {
-    return *this+ve*(-1);
+    if(this->size!=ve.size)
+        cout<<"wrong"<<endl;
+    Vector<T> div(size);
+    for(int i=0;i<size;i++)
+        div.num[i]=this->num[i]-ve.num[i];
+    return div;
 }
 
 template <typename T>
@@ -195,15 +204,14 @@ Vector<T> & Vector<T>::operator=(const Vector<T> &ve)//深赋值
     {
         if(this->num)delete []num;
         this->num=new T[ve.size];
+        this->size=ve.size;
         for(int i=0;i<size;i++)
             this->num[i]=ve.num[i];
-        this->size=ve.size;
         return *this;
     }
     else
         return *this;
 }
-
 
 //功能
 template <typename T>
@@ -232,5 +240,46 @@ bool Vector<T>::empty() const//判断是否为空向量
         return false;
 }
 
+template <typename T>
+void Vector<T>::vdimup(const int x)
+{
+    Vector<T> new_num(this->size+x);
+    for(int i=0;i<this->size+x;i++)
+    {
+        if(i<this->size)
+            new_num.num[i]=this->num[i];
+        else
+            new_num.num[i]=0;
+    }
+    *this=new_num;
+}
 
-#endif //XIANGMUSHIYAN_VECTOR_H
+template <typename T>
+void Vector<T>::vdimdown(const int x)
+{
+    Vector<T> new_num(this->size-x);
+    for(int i=0;i<this->size-x;i++)
+        new_num.num[i]=this->num[i];
+    *this=new_num;
+}
+
+template <typename T>
+double Vector<T>::angle(const Vector<T> &ve)
+{
+    double cos_;
+    cos_=cos(this->multiply(ve)/(this->module()*ve.module()));
+    return acos(cos_)*57.3;
+}
+
+template <typename T>
+T Vector<T>::multiply(const Vector<T> &ve)throw(int)
+{
+    if(this->size!=ve.size)
+        throw (int)1;
+    T sum=0;
+    for(int i=0;i<size;i++)
+        sum=sum+this->num[i]+ve.num[i];
+    return sum;
+}
+#endif //VECFINAL_VEC_H
+
