0fc107060126134915fecd2f46da59b304ee6339
shudorcl <1985366171@qq.com>
2020-11-10 22:59:17 +0800

    fix

--- Pro Lin/main.cpp
+++ Pro Lin/main.cpp
@@ -28,13 +28,11 @@ public:
 };
 int main()
 {
-    Book book1("book1", 1), book2("book2", 2), book3("book3", 3);
-    Book bookarray[10];
-    bookarray[1]=book1;
-    LinkList<Book> list(1,bookarray);
-    list.Append(book2);
-    list.Append(book3);
-    string name, choose, correct_name;
+    Book book1("BravoDragon", 1), book2("LionSeki", 2), book3("LampDoAlangina", 3);
+    Book bookarray[3]={book1,book2,book3};
+    LinkList<Book> list(3,bookarray);
+    string name, correct_name;
+    char choose;
     int id, correct_id;
     cout<<"=================\n"
         <<"Welcome to the library management system, press Enter to exit\n"<<endl;
@@ -43,14 +41,14 @@ int main()
         cout<<"List of books:"<<endl;
         list.ShowList();
         cout<<"What do you need? (1 add book. 2 borrow book. 3 book title check id. 4id check book title. 5 errata.)"<<endl;
-        getline(cin,choose);
-        if(choose[0]=='\0')
+        cin>>choose;
+        if(choose=='\0')
         {
             cout<<"See you next time!\n"
                 <<"=================\n"<<endl;
             break;
         }
-        else if(choose[0]=='1') {
+        else if(choose=='1') {
             cout << "Please enter the book name: " << endl;
             cin >> name;
             cout << "Please enter the book number: " << endl;
@@ -63,10 +61,10 @@ int main()
             list.Append(new_book);
             cout << "bibliography " + name + " Has been stored, the id of the book is: " << id << endl;
         }
-        else if(choose[0]=='2'){
+        else if(choose=='2'){
             cout << "Enter 1 book name to borrow the book, enter 2id to borrow the book" << endl;
-            getline(cin, choose);
-            if (choose[0]=='1'){
+            cin>>choose;
+            if (choose=='1'){
                 cout << "Please enter the title: " << endl;
                 cin >> name;
                 if(not list.Duplicate_name(name)){
@@ -75,7 +73,7 @@ int main()
                 }
                 list.NameDelete(name);
             }
-            else if (choose[0]=='2'){
+            else if (choose=='2'){
                 cout << "Please enter id:" << endl;
                 cin >> id;
                 if(not list.Duplicate_id(id)){
@@ -88,30 +86,30 @@ int main()
                 cout << "I suspect that you have a problem here." <<endl;
             }
         }
-        else if(choose[0]=='3'){
+        else if(choose=='3'){
             cout<<"Please enter the title:"<<endl;
             cin>>name;
-            cout << &"The id of this book is: " [ list.FindId(name)]<<endl;
+            cout << "The id of this book is: "<< list.FindId(name)<<endl;
         }
-        else if (choose[0]=='4'){
+        else if (choose=='4'){
             cout<<"Please enter id"<<endl;
             cin>>id;
             cout << "This book is titled: " + list.FindName(id)<<endl;
         }
-        else if (choose[0]=='5'){
+        else if (choose=='5'){
             cout <<"What type of errata? 1id Errata 2 Title Errata"<<endl;
             cin >> choose;
-            if (choose[0]=='1'){
+            if (choose=='1'){
                 cout << "Please enter the book title and original id: "<<endl;
                 cin >> name >> id;
                 cout << "Please enter the correct id: "<<endl;
                 cin >> correct_id;
                 list.ChangeId(name, id, correct_id);
             }
-            if (choose[0]=='1'){
+            if (choose=='2'){
                 cout << "Please enter the original title and id: "<<endl;
                 cin >> name >> id;
-                cout << "Please enter the original title and id: "<<endl;
+                cout << "Please enter the correct title: "<<endl;
                 cin >> correct_name;
                 list.ChangeName(name, id, correct_name);
             }
